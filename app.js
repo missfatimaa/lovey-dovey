1ee99f0e76fd9fe91318d8b6290532fc4898116d
missfatimaa <fatemehjaafari3672@gmail.com>
2025-02-08 23:53:34 +0330

    changing txt

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
+let txt = document.getElementById("txt");
+setInterval(txtFunc, 800);
+function txtFunc() {
+  txt.addEventListener("mouseover", function () {
+    txt.innerText = `روحم با تو نجوا می‌کند
+    و چشمانم به دنبال گام‌هایت روان است
+    هیچ چیز جز امیدم مرا آرام نمی‌کند
+    که روزی تو را ببینم`;
+  });
+}
 
 // scrollContainer.addEventListener("wheel", (event) => {
 //   event.preventDefault();
