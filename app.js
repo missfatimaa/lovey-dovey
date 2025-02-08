d2d5ec1b34907ff6b4edd553d3428f2d43b8591f
missfatimaa <fatemehjaafari3672@gmail.com>
2025-02-09 00:26:58 +0330

    finihxhin txt

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 let txt = document.getElementById("txt");
 setInterval(txtFunc, 800);
+window.addEventListener("load", function () {
+  txt.innerText = `إِن رُوحِي تُنَاجِيكِ
+وَعَيْنِي تَسِيرُ إِثْرَ خُطَاكِ
+لَمْ يَشْفِنِي سِوَى أَمَلِي
+أَنَّنِي يَوْمًا أَرَاكِ`;    
+});
 function txtFunc() {
   txt.addEventListener("mouseover", function () {
     txt.innerText = `روحم با تو نجوا می‌کند
     و چشمانم به دنبال گام‌هایت روان است
     هیچ چیز جز امیدم مرا آرام نمی‌کند
     که روزی تو را ببینم`;
+  });
+  txt.addEventListener("mouseleave", function () {
+    txt.innerText = `إِن رُوحِي تُنَاجِيكِ
+وَعَيْنِي تَسِيرُ إِثْرَ خُطَاكِ
+لَمْ يَشْفِنِي سِوَى أَمَلِي
+أَنَّنِي يَوْمًا أَرَاكِ`;
   });
 }
 
