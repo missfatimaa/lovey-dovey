f08035a15f236491047d0a50fcbc438fb7ed68d0
missfatimaa <fatemehjaafari3672@gmail.com>
2025-02-09 00:40:28 +0330

    flags

--- app.js
+++ app.js
@@ -2,27 +2,29 @@ let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 let txt = document.getElementById("txt");
-setInterval(txtFunc, 800);
+let flag = 0;
 window.addEventListener("load", function () {
   txt.innerText = `إِن رُوحِي تُنَاجِيكِ
 وَعَيْنِي تَسِيرُ إِثْرَ خُطَاكِ
 لَمْ يَشْفِنِي سِوَى أَمَلِي
-أَنَّنِي يَوْمًا أَرَاكِ`;    
-});
-function txtFunc() {
-  txt.addEventListener("mouseover", function () {
-    txt.innerText = `روحم با تو نجوا می‌کند
-    و چشمانم به دنبال گام‌هایت روان است
-    هیچ چیز جز امیدم مرا آرام نمی‌کند
-    که روزی تو را ببینم`;
-  });
-  txt.addEventListener("mouseleave", function () {
-    txt.innerText = `إِن رُوحِي تُنَاجِيكِ
-وَعَيْنِي تَسِيرُ إِثْرَ خُطَاكِ
-لَمْ يَشْفِنِي سِوَى أَمَلِي
 أَنَّنِي يَوْمًا أَرَاكِ`;
-  });
-}
+});
+
+txt.addEventListener("click", function () {
+  if ((flag == 0)) {
+    txt.innerText = `روحم با تو نجوا می‌کند
+        و چشمانم به دنبال گام‌هایت روان است
+        هیچ چیز جز امیدم مرا آرام نمی‌کند
+        که روزی تو را ببینم`;
+    flag = 1;
+  } else {
+    txt.innerText = `إِن رُوحِي تُنَاجِيكِ
+        وَعَيْنِي تَسِيرُ إِثْرَ خُطَاكِ
+        لَمْ يَشْفِنِي سِوَى أَمَلِي
+        أَنَّنِي يَوْمًا أَرَاكِ`;
+    flag = 0;
+  }
+});
 
 // scrollContainer.addEventListener("wheel", (event) => {
 //   event.preventDefault();
